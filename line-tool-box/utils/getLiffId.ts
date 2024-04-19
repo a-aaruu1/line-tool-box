987dabcba92b7eee4c85057cb6efd8e25a83e3d0
a-aaruu1 <146954010+a-aaruu1@users.noreply.github.com>
2024-04-19 20:23:09 +0900

    Update getLiffId.ts

--- line-tool-box/utils/getLiffId.ts
+++ line-tool-box/utils/getLiffId.ts
@@ -1,5 +1,5 @@
 export function getLiffId(): string {
   // const query = new URLSearchParams(window.location.search).get("liffId");
-  const liffId = "2001110580-p8w3dBZq"; // ブラウザ動作含め
+  const liffId = "2004693304-kl5BjM6X"; // ブラウザ動作含め
   return liffId;
 }
